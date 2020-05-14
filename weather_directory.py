c3768bd9daf4ce2d2b931c74af2eb9bdeb4fffbc
James-Leon <james_leon@icloud.com>
2020-05-14 06:38:10 -0400

    Added weather report entry

--- weather_directory.py
+++ weather_directory.py
@@ -12,3 +12,14 @@ def fetch_weather(woeid):
     return requests.get(API_root + API_weather + str(woeid)).json()
 
 
+def display_weather(weather):
+    print(f"Weather for {weather['title']}:")
+    for entry in weather['consolidated_weather']:
+        date = entry['application_date']
+        high = entry['max_temp']
+        low = entry['min_temp']
+        state = entry['weather_state_name']
+        print(f"{date}\t{state}\thigh {high:2.1f}°C\tlow {low:2.1f}°C")
+    
+
+        
